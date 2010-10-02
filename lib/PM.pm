5e634ee12b29d8f5a3066f238a5aa39b4ac95bb5
punytan <punytan@gmail.com>
2010-10-03 07:55:47 +0900

    initial commit

---
+++ lib/PM.pm
@@ -0,0 +1,32 @@
+package PM;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+PM -
+
+=head1 SYNOPSIS
+
+  use PM;
+
+=head1 DESCRIPTION
+
+PM is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
