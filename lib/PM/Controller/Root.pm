b63b7701e5aad5214287a19fd89b1781044470cc
punytan <punytan@gmail.com>
2010-10-07 01:34:47 +0900

    added: show random video list in /root

--- lib/PM/Controller/Root.pm
+++ lib/PM/Controller/Root.pm
@@ -3,6 +3,7 @@ use common::sense;
 our $VERSION = '0.01';
 use parent 'PM::Controller';
 
+use PM::Model::Root;
 use PM::Model::Session;
 
 my $view = PM::View->new;
@@ -12,8 +13,22 @@ sub get {
     my $env  = shift;
 
     if (PM::Model::Session->is_login($env)) {
+
+        my @rows = PM::Model::Root->fetch_random_vid;
+
+        my %vinfo;
+        for my $row (@rows) {
+            my $value = PM::Model::Root->fetch_video_info($row);
+            $vinfo{$row} = $value;
+            if ($row =~ /[s|n]m(\d+)/) {
+                $vinfo{$row}->{thumbid} = $1;
+            } else {
+                #$vinfo{$row}{thumbid} = $row;
+            }
+        }
+
         my $body = $view->render_with_encode('root_login.tx', {
-            session => $env->{'psgix.session'}});
+            session => $env->{'psgix.session'}, random => \%vinfo});
 
         return [200, ['Content-Type' => 'text/html'], [$body]];
         
--- lib/PM/Model/Root.pm
+++ lib/PM/Model/Root.pm
@@ -3,6 +3,33 @@ use common::sense;
 our $VERSION = '0.01';
 use parent 'PM::Model';
 
+use Try::Tiny;
+
+my $model = PM::Model->new;
+
+sub fetch_random_vid {
+    my $self = shift;
+
+    my $sth = $model->dbh->prepare(q{
+        SELECT vid FROM usertags ORDER BY rand() limit 20});
+    $sth->execute;
+
+    my $rows = $sth->fetchall_arrayref;
+
+    my @vids;
+    for my $row (@$rows) {
+        push @vids, $row->[0];
+    }
+    return @vids;
+}
+
+sub fetch_video_info {
+    my $self = shift;
+    my $vid  = shift;
+
+    return PM::Model::Add::SM->fetch_video_info($vid);
+}
+
 1;
 __END__
 
